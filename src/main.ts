970c045cc8703231216a7b18d69bb2e7e2ed8586
JeanHermance <jeanhermace@gmail.com>
2026-04-21 11:04:31 +0300

    route api

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('',app, document);
+  SwaggerModule.setup('api',app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch(console.error);
